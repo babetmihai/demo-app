56e9db23e2bb7f87810da54a74efe44c65cce6fb
Mihai Babet <mihai.babet@gmail.com>
2020-03-24 23:58:06 +0200

    json editor

--- src/routes/Editor/constants.js
+++
@@ -1,7 +0,0 @@
-export const TEXT = 'TEXT'
-export const NUMBER = 'NUMBER'
-export const COMPOUND = 'COMPOUND'
-export const SELECT = 'SELECT'
-export const DATE = 'DATE'
-export const MEASUREMENT = 'MEASUREMENT'
-export const MULTILANGUAGE = 'MULTILANGUAGE'
--- src/routes/Editor/index.jsx
+++ src/routes/Editor/index.jsx
@@ -1,102 +1,74 @@
 import React, { PureComponent } from 'react'
-import _ from 'lodash'
 import { connect } from 'react-redux'
+import _ from 'lodash'
 import store from 'store/actions'
-import { Button } from 'react-bootstrap'
-import {
-  formInit,
-  formChange,
-  formSubmit,
-  formRevert,
-  formDownload
-} from './actions'
-import FormValue from './FormValue'
-import styles from './index.module.scss'
 import Page from 'layout/Page'
-import { t } from 'core/intl'
+import styles from './index.module.scss'
 
 class FormComponent extends PureComponent {
 
-  componentDidMount() {
-    const { value } = this.props
-    if (_.isEmpty(value)) formInit()
-  }
-
   render() {
-    const {
-      value,
-      initialValue,
-      error,
-      loading,
-      dictionary
-    } = this.props
-
     return (
-
       <Page
         loading={!value}
         className={styles.editor}
       >
-        <div className={styles.header}>
-          <Button
-            variant="light"
-            onClick={this.handleSubmit}
-            type="submit"
-          >
-            <i>keyboard_return</i>
-          </Button>
-          <Button
-            variant="light"
-            onClick={this.handleRevert}
-          >
-            <i>undo</i>
-          </Button>
-          <Button
-            variant="light"
-            onClick={this.handleDownload}
-          >
-            <i>get_app</i>
-          </Button>
-        </div>
-        <div className={styles.content}>
-          <FormValue
-            label={t('editor')}
-            value={value}
-            initialValue={initialValue}
-            error={error}
-            loading={loading}
-            dictionary={dictionary}
-            onChange={this.handleChange}
-          />
-        </div>
+        <Resolver value={value} />
       </Page>
     )
   }
+}
 
-  handleChange = ({ name, value, path }) => {
-    const { dictionary } = this.props
-    return formChange({ name, value, path, dictionary })
+function Resolver({ value }) {
+  switch (true) {
+    case (_.isPlainObject(value)): {
+      return (
+        <div>
+          {Object.keys(value).map((id) => (
+            <Resolver
+              key={id}
+              value={value[id]}
+            />
+          ))}
+        </div>
+      )
+    }
+    case (_.isArray(value)): {
+      return (
+        <div>
+          {Object.values(value).map((item, index) => (
+            <Resolver key={index} value={item} />
+          ))}
+        </div>
+      )
+    }
+    default: {
+      return (
+        <div>
+          {value}
+        </div>
+      )
+    }
   }
+}
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    const { value, dictionary } = this.props
-    return formSubmit({ value, dictionary })
-  }
-
-  handleRevert = (event) => {
-    event.preventDefault()
-    const { initialValue } = this.props
-    return formRevert({ initialValue })
-  }
-
-  handleDownload = (event) => {
-    event.preventDefault()
-    const { value } = this.props
-    return formDownload({
-      value,
-      name: t('editor')
-    })
+export const value = {
+  name: 'M34234',
+  age: 123,
+  email: [
+    'test2@gmail.com',
+    'test1@gmail.com'
+  ],
+  role: [
+    'admin',
+    'user'
+  ],
+  section: {
+    name: '123',
+    date: '2019-01-15',
+    price: '123',
+    weight: '123',
+    description: 'this is a textarea'
   }
 }
 
