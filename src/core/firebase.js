642d87b5635537dd2cc1171b3565f02a51c84725
Mihai Babet <mihai.babet@gmail.com>
2021-04-14 12:31:51 +0300

    fix

--- src/core/firebase.js
+++ src/core/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBlz1clTHLvNYkX3DdLuS3NahX2-kJookQ",
-  authDomain: "babet-mihai.firebaseapp.com",
-  databaseURL: "https://babet-mihai.firebaseio.com",
-  projectId: "babet-mihai",
-  storageBucket: "babet-mihai.appspot.com",
-  messagingSenderId: "897097348817",
-  appId: "1:897097348817:web:907218278da2fff61077f0"
-};)
+  apiKey: 'AIzaSyBlz1clTHLvNYkX3DdLuS3NahX2-kJookQ',
+  authDomain: 'babet-mihai.firebaseapp.com',
+  databaseURL: 'https://babet-mihai.firebaseio.com',
+  projectId: 'babet-mihai',
+  storageBucket: 'babet-mihai.appspot.com',
+  messagingSenderId: '897097348817',
+  appId: '1:897097348817:web:907218278da2fff61077f0'
+})
 
 export default firebase
